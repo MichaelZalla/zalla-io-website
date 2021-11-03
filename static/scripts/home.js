e3f6631d29515fe07ff04efde6f22897ecdba826
Michael Zalla <michael@zalla.io>
2021-11-02 19:11:36 -0700

    Supports synchronous and asynchronous load;

--- static/scripts/home.js
+++ static/scripts/home.js
@@ -1,306 +1,320 @@
-window.onload = () => {
+(() => {
 
-	Fizz.setEnv('prod');
-
-	const demo = new Fizz.Demo('#canvas');
-
-	// Static configuration
-
-	const CanvasWidth = demo.stage.width;
-
-	const CanvasHeight = demo.stage.height;
-
-	const GenerationLifetime = 1000 / 8;
-
-	const InitialCellLife = GenerationLifetime + 500;
-
-	// Dynamic configuration
-
-	let rowCount;
-	let columnCount;
-	let cellWidth;
-	let cellHeight;
-	let nextGenerationState;
-	let grid;
-	let cells;
-	let timeoutId;
-
-	function init(
-		resolution)
+	if(/complete|interactive|loaded/.test(document.readyState))
 	{
-
-		if(typeof cancelTimeout !== 'undefined')
-		{
-			window.cancelTimeout(timeoutId);
-		}
-
-		rowCount = CanvasWidth * resolution;
-		columnCount = CanvasHeight * resolution;
-
-		cellWidth = Math.floor(CanvasWidth / rowCount);
-		cellHeight = Math.floor(CanvasHeight / rowCount);
-
-		nextGenerationState = makeArray(rowCount * columnCount, false);
-
-		// Initializes our cell grid (container)
-
-		grid = new Fizz.DisplayGrid({
-			rows: rowCount,
-			columns: columnCount,
-			cellWidth: cellWidth,
-			cellHeight: cellHeight,
-		});
-
-		cells = makeCells(
-			rowCount * columnCount,
-			cellWidth,
-			cellHeight,
-			InitialCellLife
-		);
-
-		grid.addChild(cells);
-
-		demo.pause();
-
-		demo.stage.empty();
-
-		demo.stage.addChild(grid);
-
-		demo.play();
-
-		(function generate() {
-
-			evolve();
-
-			timeoutId = window.setTimeout(generate, GenerationLifetime);
-
-		}());
-
+		init();
+	}
+	else
+	{
+		window.addEventListener('DOMContentLoaded', init);
 	}
 
-	function makeArray(
-		defaultValue,
-		size)
+	function init()
 	{
 
-		const array = [];
+		Fizz.setEnv('prod');
 
-		for(let i = 0; i < size; i++)
+		const demo = new Fizz.Demo('#canvas');
+
+		// Static configuration
+
+		const CanvasWidth = demo.stage.width;
+
+		const CanvasHeight = demo.stage.height;
+
+		const GenerationLifetime = 1000 / 8;
+
+		const InitialCellLife = GenerationLifetime + 500;
+
+		// Dynamic configuration
+
+		let rowCount;
+		let columnCount;
+		let cellWidth;
+		let cellHeight;
+		let nextGenerationState;
+		let grid;
+		let cells;
+		let timeoutId;
+
+		function init(
+			resolution)
 		{
-			array.push(defaultValue);
-		}
 
-		return array;
+			if(typeof cancelTimeout !== 'undefined')
+			{
+				window.cancelTimeout(timeoutId);
+			}
 
-	}
+			rowCount = CanvasWidth * resolution;
+			columnCount = CanvasHeight * resolution;
 
-	function makeCells(
-		count,
-		width,
-		height,
-		maxLife)
-	{
+			cellWidth = Math.floor(CanvasWidth / rowCount);
+			cellHeight = Math.floor(CanvasHeight / rowCount);
 
-		// Generates cells with a randomized start configuration
+			nextGenerationState = makeArray(rowCount * columnCount, false);
 
-		const cells = [];
+			// Initializes our cell grid (container)
 
-		for(let i = 0; i < count; i++)
-		{
-
-			let cell = new Fizz.DisplayEntity({
-				size: [width, height],
+			grid = new Fizz.DisplayGrid({
+				rows: rowCount,
+				columns: columnCount,
+				cellWidth: cellWidth,
+				cellHeight: cellHeight,
 			});
 
-			cell.caching = true;
+			cells = makeCells(
+				rowCount * columnCount,
+				cellWidth,
+				cellHeight,
+				InitialCellLife
+			);
 
-			cell.fillStyle = Fizz.Color.BLACK;
+			grid.addChild(cells);
 
-			cell.strokeStyle = Fizz.Color.CLEAR;
+			demo.pause();
 
-			cell.life = (Fizz.math.randomInt(2) === 1) ?
-				maxLife :
-				0;
+			demo.stage.empty();
 
-			cells.push(cell);
+			demo.stage.addChild(grid);
+
+			demo.play();
+
+			(function generate() {
+
+				evolve();
+
+				timeoutId = window.setTimeout(generate, GenerationLifetime);
+
+			}());
 
 		}
 
-		return cells;
-
-	}
-
-	function getNeighborsCount(
-		x,
-		y)
-	{
-
-		// const neighbors = [
-		// 	null, null, null,
-		// 	null, null, null,
-		// 	null, null, null,
-		// ];
-
-		const isFirstX = (x === 0);
-		const isLastX = (x === (rowCount - 1));
-		const isFirstY = (y === 0);
-		const isLastY = (y === (columnCount - 1));
-
-		// Collects neighbor (or null) references
-
-		const topLeftNeighbor = grid.childAtCoordinate(x-1, y-1);
-		const topMiddleNeighbor = grid.childAtCoordinate(x, y-1);
-		const topRightNeighbor = grid.childAtCoordinate(x+1, y-1);
-
-		const middleLeftNeighbor = grid.childAtCoordinate(x-1, y);
-		const middleRightNeighbor = grid.childAtCoordinate(x+1, y);
-
-		const bottomLeftNeighbor = grid.childAtCoordinate(x-1, y+1);
-		const bottomMiddleNeighbor = grid.childAtCoordinate(x, y+1);
-		const bottomRightNeighbor = grid.childAtCoordinate(x+1, y+1);
-
-		let neighborsCount = 0;
-
-		// Checks top left
-
-		if(topLeftNeighbor && topLeftNeighbor.life > 0 && !isFirstY && !isFirstX)
+		function makeArray(
+			defaultValue,
+			size)
 		{
-			// neighbors[0] = topLeftNeighbor;
-			neighborsCount += 1;
+
+			const array = [];
+
+			for(let i = 0; i < size; i++)
+			{
+				array.push(defaultValue);
+			}
+
+			return array;
+
 		}
 
-		// Checks top middle
-
-		if(topMiddleNeighbor && topMiddleNeighbor.life > 0 && !isFirstY)
+		function makeCells(
+			count,
+			width,
+			height,
+			maxLife)
 		{
-			// neighbors[1] = topMiddleNeighbor;
-			neighborsCount += 1;
-		}
 
-		// Checks top right
+			// Generates cells with a randomized start configuration
 
-		if(topRightNeighbor && topRightNeighbor.life > 0 && !isFirstY && !isLastX)
-		{
-			// neighbors[2] = topRightNeighbor;
-			neighborsCount += 1;
-		}
+			const cells = [];
 
-		// Checks middle left
-
-		if(middleLeftNeighbor && middleLeftNeighbor.life > 0 && !isFirstX)
-		{
-			// neighbors[3] = middleLeftNeighbor;
-			neighborsCount += 1;
-		}
-
-		// Checks middle right
-
-		if(middleRightNeighbor && middleRightNeighbor.life > 0 && !isLastX)
-		{
-			// neighbors[4] = middleRightNeighbor;
-			neighborsCount += 1;
-		}
-
-		// Checks bottom left
-
-		if(bottomLeftNeighbor && bottomLeftNeighbor.life > 0 && !isLastY && !isFirstX)
-		{
-			// neighbors[5] = bottomLeftNeighbor;
-			neighborsCount += 1;
-		}
-
-		// Checks bottom middle
-
-		if(bottomMiddleNeighbor && bottomMiddleNeighbor.life > 0 && !isLastY)
-		{
-			// neighbors[6] = bottomMiddleNeighbor;
-			neighborsCount += 1;
-		}
-
-		// Checks bottom right
-
-		if(bottomRightNeighbor && bottomRightNeighbor.life > 0 && !isLastY && !isLastX)
-		{
-			// neighbors[7] = bottomRightNeighbor;
-			neighborsCount += 1;
-		}
-
-		// return neighbors.filter((n) => n !== null);
-		return neighborsCount;
-
-	}
-
-	function updateNextGenerationState()
-	{
-
-		for(let y = 0; y < columnCount; y++)
-		{
-			for(let x = 0; x < rowCount; x++)
+			for(let i = 0; i < count; i++)
 			{
 
-				const cell = grid.childAtCoordinate(x, y);
+				let cell = new Fizz.DisplayEntity({
+					size: [width, height],
+				});
 
-				const neighborsCount = getNeighborsCount(x, y);
+				cell.caching = true;
 
-				const index = (y * rowCount) + x;
+				cell.fillStyle = Fizz.Color.BLACK;
 
-				// 1. Any live cell with two or three live neighbours survives;
-				// 2. Any dead cell with three live neighbours becomes a live cell;
-				// 3. All other live cells die in the next generation. Similarly, all other dead cells stay dead;
+				cell.strokeStyle = Fizz.Color.CLEAR;
 
-				let shouldLiveToNextGeneration = (cell.life > 0) ?
-					(neighborsCount === 2 || neighborsCount === 3) :
-					(neighborsCount === 3);
+				cell.life = (Fizz.math.randomInt(2) === 1) ?
+					maxLife :
+					0;
 
-				// if(Fizz.math.randomInt(24) === 8)
-				// {
-				// 	shouldLiveToNextGeneration = true;
-				// }
-
-				nextGenerationState[index] = shouldLiveToNextGeneration;
+				cells.push(cell);
 
 			}
 
+			return cells;
+
 		}
 
-	}
-
-	function evolve()
-	{
-
-		updateNextGenerationState();
-
-		for(let y = 0; y < columnCount; y++)
+		function getNeighborsCount(
+			x,
+			y)
 		{
-			for(let x = 0; x < rowCount; x++)
+
+			// const neighbors = [
+			// 	null, null, null,
+			// 	null, null, null,
+			// 	null, null, null,
+			// ];
+
+			const isFirstX = (x === 0);
+			const isLastX = (x === (rowCount - 1));
+			const isFirstY = (y === 0);
+			const isLastY = (y === (columnCount - 1));
+
+			// Collects neighbor (or null) references
+
+			const topLeftNeighbor = grid.childAtCoordinate(x-1, y-1);
+			const topMiddleNeighbor = grid.childAtCoordinate(x, y-1);
+			const topRightNeighbor = grid.childAtCoordinate(x+1, y-1);
+
+			const middleLeftNeighbor = grid.childAtCoordinate(x-1, y);
+			const middleRightNeighbor = grid.childAtCoordinate(x+1, y);
+
+			const bottomLeftNeighbor = grid.childAtCoordinate(x-1, y+1);
+			const bottomMiddleNeighbor = grid.childAtCoordinate(x, y+1);
+			const bottomRightNeighbor = grid.childAtCoordinate(x+1, y+1);
+
+			let neighborsCount = 0;
+
+			// Checks top left
+
+			if(topLeftNeighbor && topLeftNeighbor.life > 0 && !isFirstY && !isFirstX)
 			{
+				// neighbors[0] = topLeftNeighbor;
+				neighborsCount += 1;
+			}
 
-				const cell = grid.childAtCoordinate(x, y);
+			// Checks top middle
 
-				const index = (y * rowCount) + x;
+			if(topMiddleNeighbor && topMiddleNeighbor.life > 0 && !isFirstY)
+			{
+				// neighbors[1] = topMiddleNeighbor;
+				neighborsCount += 1;
+			}
 
-				const shouldLive = nextGenerationState[index];
+			// Checks top right
 
-				cell.life = (shouldLive) ?
-					InitialCellLife :
-					0;
+			if(topRightNeighbor && topRightNeighbor.life > 0 && !isFirstY && !isLastX)
+			{
+				// neighbors[2] = topRightNeighbor;
+				neighborsCount += 1;
+			}
 
-				if(cell.life > 0)
+			// Checks middle left
+
+			if(middleLeftNeighbor && middleLeftNeighbor.life > 0 && !isFirstX)
+			{
+				// neighbors[3] = middleLeftNeighbor;
+				neighborsCount += 1;
+			}
+
+			// Checks middle right
+
+			if(middleRightNeighbor && middleRightNeighbor.life > 0 && !isLastX)
+			{
+				// neighbors[4] = middleRightNeighbor;
+				neighborsCount += 1;
+			}
+
+			// Checks bottom left
+
+			if(bottomLeftNeighbor && bottomLeftNeighbor.life > 0 && !isLastY && !isFirstX)
+			{
+				// neighbors[5] = bottomLeftNeighbor;
+				neighborsCount += 1;
+			}
+
+			// Checks bottom middle
+
+			if(bottomMiddleNeighbor && bottomMiddleNeighbor.life > 0 && !isLastY)
+			{
+				// neighbors[6] = bottomMiddleNeighbor;
+				neighborsCount += 1;
+			}
+
+			// Checks bottom right
+
+			if(bottomRightNeighbor && bottomRightNeighbor.life > 0 && !isLastY && !isLastX)
+			{
+				// neighbors[7] = bottomRightNeighbor;
+				neighborsCount += 1;
+			}
+
+			// return neighbors.filter((n) => n !== null);
+			return neighborsCount;
+
+		}
+
+		function updateNextGenerationState()
+		{
+
+			for(let y = 0; y < columnCount; y++)
+			{
+				for(let x = 0; x < rowCount; x++)
 				{
-					cell.exists = true;
+
+					const cell = grid.childAtCoordinate(x, y);
+
+					const neighborsCount = getNeighborsCount(x, y);
+
+					const index = (y * rowCount) + x;
+
+					// 1. Any live cell with two or three live neighbours survives;
+					// 2. Any dead cell with three live neighbours becomes a live cell;
+					// 3. All other live cells die in the next generation. Similarly, all other dead cells stay dead;
+
+					let shouldLiveToNextGeneration = (cell.life > 0) ?
+						(neighborsCount === 2 || neighborsCount === 3) :
+						(neighborsCount === 3);
+
+					// if(Fizz.math.randomInt(24) === 8)
+					// {
+					// 	shouldLiveToNextGeneration = true;
+					// }
+
+					nextGenerationState[index] = shouldLiveToNextGeneration;
+
 				}
 
 			}
+
 		}
+
+		function evolve()
+		{
+
+			updateNextGenerationState();
+
+			for(let y = 0; y < columnCount; y++)
+			{
+				for(let x = 0; x < rowCount; x++)
+				{
+
+					const cell = grid.childAtCoordinate(x, y);
+
+					const index = (y * rowCount) + x;
+
+					const shouldLive = nextGenerationState[index];
+
+					cell.life = (shouldLive) ?
+						InitialCellLife :
+						0;
+
+					if(cell.life > 0)
+					{
+						cell.exists = true;
+					}
+
+				}
+			}
+
+		}
+
+		// Initialization
+
+		init(0.125);
+
+		window.setTimeout(init.bind(null, 0.25), 5000);
+
+		window.setTimeout(init.bind(null, 0.5), 10000);
 
 	}
 
-	// Initialization
-
-	init(0.125);
-
-	window.setTimeout(init.bind(null, 0.25), 5000);
-
-	window.setTimeout(init.bind(null, 0.5), 10000);
-
-};
+})();
