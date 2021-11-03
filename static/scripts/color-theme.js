48fb9d77e1e96c2497c7c751e63e06ef796735f4
Michael Zalla <michael@zalla.io>
2021-11-02 19:18:11 -0700

    Adds color theme picker;
    Adds glyphs to `zalla-io-font`; updates icon classes; adds color theme script;

---
+++ static/scripts/color-theme.js
@@ -0,0 +1,87 @@
+(() => {
+
+	if(/complete|interactive|loaded/.test(document.readyState))
+	{
+		init();
+	}
+	else
+	{
+		window.addEventListener('DOMContentLoaded', init);
+	}
+
+	function init()
+	{
+
+		const DefaultThemeOnLoad = 'light';
+
+		const LocalStorageKey = 'preferences.theme.preferred';
+
+		let currentTheme = DefaultThemeOnLoad;
+
+		function setTheme(
+			theme)
+		{
+
+			if(theme !== currentTheme)
+			{
+
+				themedContainers.map((elem) => {
+
+					if(elem.classList.contains('theme-light'))
+					{
+						elem.classList.replace('theme-light', 'theme-dark');
+					}
+					else
+					{
+						elem.classList.replace('theme-dark', 'theme-light');
+					}
+
+				});
+
+			}
+
+			currentTheme = theme;
+
+			themeTriggers.map((trigger) => {
+
+				if(trigger.dataset.theme === currentTheme)
+				{
+					trigger.setAttribute('disabled', 'disabled');
+				}
+				else
+				{
+					trigger.removeAttribute('disabled');
+				}
+
+			});
+
+			window.localStorage.setItem(LocalStorageKey, currentTheme);
+
+		}
+
+		const themedContainers = Array.from(
+			document.body.querySelectorAll('[class*=\'theme-\']')
+		);
+
+		const themeTriggers = Array.from(
+			document.body.querySelectorAll('[data-theme]')
+		);
+
+		let preferredTheme = window.localStorage.getItem(LocalStorageKey);
+
+		if(preferredTheme && preferredTheme !== DefaultThemeOnLoad)
+		{
+			setTheme(preferredTheme);
+		}
+		else
+		{
+			setTheme(DefaultThemeOnLoad);
+		}
+
+		themeTriggers.map((trigger) => trigger.addEventListener('click', (e) => {
+			setTheme(trigger.dataset.theme);
+		}));
+
+	}
+
+})();
