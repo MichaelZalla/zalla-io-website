9376d1d8e7545748cfc3cf55b99eb3a6a8691291
Michael Zalla <michael@zalla.io>
2021-11-06 00:50:08 -0700

    Removes `normalize.css` dependency;

--- postinstall.sh
+++ postinstall.sh
@@ -1,6 +1,2 @@
 # Sets default AWS profile for `hugo deploy` commands;
 export AWS_PROFILE=zalla-io-s3-manager
-
-# Copies `normalize.css` into `./static` directory
-mkdir -p ./static/css/vendor/normalize-css
-cp ./node_modules/normalize-css/normalize.css ./static/css/vendor/normalize-css/
