d58ce47e981872fe2772984a49ddf43343464c86
Michael Zalla <michael@zalla.io>
2021-11-22 21:17:29 -0800

    Auto-installs global packages;
    Creates folder if needed;

--- postinstall.sh
+++ postinstall.sh
@@ -1,6 +1,11 @@
+# Auto-installs global package CLIs
+npm i -g postcss-cli
+npm install -g autoprefixer
+
 # Sets default AWS profile for `hugo deploy` commands;
 export AWS_PROFILE=zalla-io-s3-manager
 
 # Generates light and dark-theme styles for (code) syntax highlighting
+mkdir -p ./static/css
 hugo gen chromastyles --style=monokai > ./static/css/monokai-dark.css
 hugo gen chromastyles --style=monokailight > ./static/css/monokai-light.css
